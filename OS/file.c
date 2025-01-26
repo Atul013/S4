ae6ed203686b923f16b4cd0ec6733bfdc2c8fdfb
Atul Biju <141022002+Atul013@users.noreply.github.com>
2025-01-26 21:51:38 +0530

    Update file.c

--- OS/file.c
+++ OS/file.c
@@ -3,27 +3,21 @@
 #include<string.h>
 #include<fcntl.h>
 
-int main()
-{
-	char wbuf[128];
-	char rbuf[128];
-	int fd = open("file.txt", O_RDWR | O_CREAT, 0644);
-	if (fd < 0)
-	{
-		perror("open");
-		return 1;
-	}
-	printf("Enter the text to be written: ");
-	scanf("%s", wbuf);
-	write(fd, wbuf, strlen(wbuf));
-	close(fd);
-	int fd2 = open("file.txt", O_RDWR);
-	printf("The Contents in the file are : \n");
-	read(fd2, rbuf, 100);
-	printf("%s\n", rbuf);
-	close(fd2);
-	return 0;
-}
+int main() {
+    char buf[128];
+    int fd = open("file.txt", O_RDWR | O_CREAT, 0644);
+    if (fd < 0) { perror("open"); return 1; }
 
-// shits still too buggy
-// i need to fix this
+    printf("Enter text: ");
+    scanf("%s", buf);
+    write(fd, buf, strlen(buf));
+    close(fd);
+
+    fd = open("file.txt", O_RDWR);
+    printf("File contents:\n");
+    read(fd, buf, sizeof(buf));
+    printf("%s\n", buf);
+    close(fd);
+
+    return 0;
+}
