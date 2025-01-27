e457f86ce882037ca4f0028cc78f761a6125a4b4
Atul Biju <141022002+Atul013@users.noreply.github.com>
2025-01-27 10:47:22 +0530

    Update process.c

--- OS/process.c
+++ OS/process.c
@@ -1,20 +1,27 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<sys/wait.h>
 int main()
 {
+	int pidc, status;
 	int pid = fork();
 	if (pid < 0)
-	{	
+	{
 		perror("fork");
 		return 1;
 	}
 	if (pid == 0)
 	{	
-		printf("Hello from Child ! (PID: %d)\n", getpid());
+		printf("Hello from Child! (PID : %d) \n",getpid());
+		execlp("/bin/date","date",NULL);
+		
 	}
 	else
-	{	
-		printf("Hello from Parent ! (PID: %d, Child PID: %d)\n", getpid(), pid);
+	{
+		pidc = wait(&status);
+		printf("\nHello from Parent class (PID: %d, childPID: %d) \n",getpid(), pid);
 	}
 	return 0;
 }
+
+
